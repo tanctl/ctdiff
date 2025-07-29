555ceca38b8d5d19e122efc228f79745aaa87e71
Tanya Arora <tanyaarora1866@gmail.com>
2025-07-29 21:24:46 +0530

    feat: cli interface and timing side-channel attack simulation framework

--- src/lib.rs
+++ src/lib.rs
@@ -10,5 +10,9 @@ pub mod primitives;
 pub mod types;
 pub mod algorithm;
 
+pub mod vulnerable;
+pub mod timing;
+pub mod attack;
+
 // convenience re-exports of common primitives
 pub use primitives::{ct_bytes_eq, ct_min, ct_max, ct_copy_if, ct_lookup, ct_memcmp};
